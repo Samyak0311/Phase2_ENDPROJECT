6932129f7cdf703b08a106dd8f412625e7b7d254
admin <admin111@gmail.com>
2024-02-01 13:54:18 +0530

    done

---
+++ ATE_Phase2_ENDPROJECT_Scenario1/src/test/java/steps/BaseTest.java
@@ -0,0 +1,27 @@
+package steps;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class BaseTest {
+public static WebDriver driver;
+	
+	@Before
+	public void initialization()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+	}
+	
+	@After
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
+
+
+}
---
+++ ATE_Phase2_ENDPROJECT_Scenario1/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"d824a024-10d5-4b1e-9e2c-caac6cfac620","feature":"Test StartHealth webpage using cucumber","scenario":"Validate the Star Health Buy Now flow","start":1706763410749,"group":1,"content":"","tags":"","end":1706763414120,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+});
